d02b618eaa23ad7e6afb996e43b634c8fe78eaa6
Mr_Pray <pratikkhanal.stan@gmail.com>
2026-01-01 13:29:19 +0545

    Initial

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Post;
+use App\Models\User;
+use App\Models\Category;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class DatabaseSeeder extends Seeder
+{
+    use WithoutModelEvents;
+
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        User::factory()->create([
+            'name'=>'John Doe',
+            'email'=> 'john@example.com',
+        ]);
+
+        $categories =[
+            'Technology',
+            'Health',
+            'Science',
+            'Sports',
+            'Politics',
+        ];
+
+        foreach($categories as $category) {
+            Category::create(['name' => $category]);
+        }
+
+        Post::factory(100)->create();
+    }
+}
